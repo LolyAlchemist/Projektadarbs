ec13ed37634615e74c8b8eea3fb2c9c66f7ec152
Lolita <148311044+LolyAlchemist@users.noreply.github.com>
2025-12-02 22:11:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ pygame.init()
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 1000
 SCROLL_HEIGHT = 1200
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("HHHHHH")
+
 game_font = pygame.font.SysFont(name="Lora", size=50)
 game_font2 = pygame.font.SysFont(name="Lora", size=25)
 
 grid = Grid(pygame, game_font)
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("HHHHHH")
 
 start_img = pygame.image.load("START_BUTTON.png").convert_alpha()
 quit_img = pygame.image.load("QUIT_BUTTON.png").convert_alpha()
@@ -33,36 +34,23 @@ state = "menu"
 run = True
 
 scrollbar = ScrollBar(1150, 0, SCREEN_HEIGHT)
-
 scroll = pygame.Surface((SCREEN_WIDTH, SCROLL_HEIGHT))
-
 scroll_offset = 0
 
 while run:
     for event in pygame.event.get():
         scrollbar.handle_event(event)
-
         if event.type == pygame.QUIT:
             run = False
-
         if state == "game" and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not grid.win:
             pos = pygame.mouse.get_pos()
             pos = (pos[0], pos[1] + scroll_offset)
             grid.get_mouse_click(pos[0], pos[1])
-
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and grid.win:
                 grid.restart()
-        if grid.win:
-            won_surface = game_font.render("Tu uzvarēji!", False, (0, 255, 0))
-            scroll.blit(won_surface, (956, 650))
 
-            press_space_surf = game_font2.render("Spied space, lai restartētu.", False, (0, 255, 200))
-            scroll.blit(press_space_surf, (920, 750))
-
-        scroll_offset = int(scrollbar.scroll_percent * (SCROLL_HEIGHT - SCREEN_HEIGHT))
-        screen.blit(scroll, (0, -scroll_offset))
-        scrollbar.draw(screen)
+    scroll_offset = int(scrollbar.scroll_percent * (SCROLL_HEIGHT - SCREEN_HEIGHT))
 
     if state == "menu":
         screen.fill((202, 228, 241))
@@ -76,36 +64,43 @@ while run:
     elif state == "game":
         scroll.fill((0, 0, 0))
         grid.draw_all(pygame, scroll)
+        if grid.win:
+            won_surface = game_font.render("Tu uzvarēji!", False, (0, 255, 0))
+            scroll.blit(won_surface, (956, 650))
+            press_space_surf = game_font2.render(
+                "Spied space, lai restartētu.", False, (0, 255, 200)
+            )
+            scroll.blit(press_space_surf, (920, 750))
+        screen.blit(scroll, (0, -scroll_offset))
+        scrollbar.draw(screen)
 
     elif state == "tutorial":
-        scroll.fill((255,255,255))
-        text = [
-    "NOTEIKUMI:",
-    "* Spēkā ir parastie sudoku noteikumi.",
-    "* Zem sudoku ir teksts, kurš informē — kurā kolonnā un rindā ir bomba.",
-    "* Zem bombas lokācijas informācijas jāievada pareizais cipars.",
-    "",
-    "BOMBAS LOKĀCIJA:",
-    "Kolonna: ____",
-    "Rinda: ____",
-    "",
-    "PAREIZAIS CIPARS (BOMBA):",
-    "____"
-    ]
-        font = pygame.font.SysFont("Arial", 25)
+        scroll.fill((255, 255, 255))
         y = 20
-
+        font = pygame.font.SysFont("Arial", 25)
+        text = [
+            "NOTEIKUMI:",
+            "* Spēkā ir parastie sudoku noteikumi.",
+            "* Zem sudoku ir teksts, kurš informē — kurā kolonnā un rindā ir bomba.",
+            "* Zem bombas lokācijas informācijas jāievada pareizais cipars.",
+            "",
+            "BOMBAS LOKĀCIJA:",
+            "Kolonna: ____",
+            "Rinda: ____",
+            "",
+            "PAREIZAIS CIPARS (BOMBA):",
+            "____"
+        ]
         for line in text:
             surface = font.render(line, True, (0, 0, 0))
             scroll.blit(surface, (20, y))
             y += 35
         if back_button.draw(scroll):
             state = "menu"
+        screen.blit(scroll, (0, -scroll_offset))
+        scrollbar.draw(screen)
+
+
     pygame.display.flip()
-
-
-
-
-    pygame.display.update()
 
 pygame.quit()
